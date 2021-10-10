1d5d69e64ae582706774719c6ed36995a3a9eaa6
Cory Piette <coryjpiette@gmail.com>
2021-10-10 16:17:37 -0400

    adding event listener to serviceWorker

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -17,3 +17,21 @@ const isLocalhost = Boolean(
           return;
       }
   
+      window.addEventListener('load', () => {
+        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+        if (isLocalhost) {
+          checkValidServiceWorker(swUrl, config);
+          navigator.serviceWorker.ready.then(() => {
+            console.log(
+              'This web app is being served cache-first by a service ' +
+                'worker. To learn more, visit https://bit.ly/CRA-PWA'
+            );
+          });
+        } else {
+      
+          registerValidSW(swUrl, config);
+        }
+      });
+    }
+  }
+  
